e0e04fb31fd1385c6fd68147bf56f22590be9236
Hadi Ahmadi <ahmadi.hadi@outlook.com>
2020-12-29 19:24:33 +0330

    add a test class

---
+++ Store/test/Sales.Domain.Tests/When_applying_discount_on_order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using FsCheck;
+using FsCheck.Xunit;
+using Xunit;
+
+namespace Sales.Domain.Tests
+{
+    public class When_applying_percentage_based_discount_on_order
+    {
+
+    }
+}
