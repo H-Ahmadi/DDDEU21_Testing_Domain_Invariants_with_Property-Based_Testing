ef8cae8e21255c0e6ce94a8fd73b2d521ae4f082
Hadi Ahmadi <ahmadi.hadi@outlook.com>
2021-01-22 10:11:30 +0330

    test for total price of order added

--- Store/test/Sales.Domain.Tests/When_calculating_total_price_of_order.cs
+++ Store/test/Sales.Domain.Tests/When_calculating_total_price_of_order.cs
@@ -18,17 +18,28 @@ namespace Sales.Domain.Tests
         public Property total_price_of_order_is_equal_to_total_price_of_item_when_there_is_only_one_item(OrderItem item)
         {
             var orderItems = new List<OrderItem> { item };
-            var order = new Order(1, orderItems);
+            var order = new Order(1, orderItems); 
             return (order.TotalPrice() == item.TotalPrice()).ToProperty();
         }
 
         [Property]
-        public Property total_price_of_order_is_sum_of_all_order_items(List<OrderItem> items)
+        public Property subtracting_item_from_total_price_of_order_results_in_other_item(OrderItem firstItem, OrderItem secondItem)
+        {
+            var items = new List<OrderItem> { firstItem, secondItem };
+            var order = new Order(1, items);
+
+            return (order.TotalPrice() - firstItem.TotalPrice() == secondItem.TotalPrice()).ToProperty();
+        }
+
+        [Property]
+        public Property subtracting_all_items_from_total_price_results_in_zero(List<OrderItem> items)
         {
             var order = new Order(1, items);
-            var totalPriceOfOrder = order.TotalPrice();
-            items.ForEach(a=> totalPriceOfOrder -= a.TotalPrice());
-            return (totalPriceOfOrder == 0).ToProperty();
+            var totalPrice = order.TotalPrice();
+
+            items.ForEach(a=> totalPrice -= a.TotalPrice());
+
+            return (totalPrice == 0).ToProperty();
         }
     }
 }
